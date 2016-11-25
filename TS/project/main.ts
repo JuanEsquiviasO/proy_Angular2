e089a163b67750cd8548a29dc97fc06b75e5148a
Juan Esquivias <juanesquiviasotazu@gmail.com>
2016-11-25 09:56:17 -0500

    Generate a project in TypeScript

---
+++ TS/project/main.ts
@@ -0,0 +1,16 @@
+import './datos';
+
+class Usuarios {
+	usuario: Usuario;
+	constructor( name: string,
+	age : number
+	){
+		this.usuario = {
+			name : name,
+			age : age
+		}
+		console.log(this.usuario);
+	}
+}
+
+const u = new Usuarios('Fokker', 30);
